5fe9f848d11cec471f0d68e03f765ddc5b7d4f58
intersense <zgw1982@gmail.com>
2016-02-06 12:14:00 -0700

    fixed parameter error

--- verify_glmem.cu
+++ verify_glmem.cu
@@ -1,8 +1,8 @@
 //jacobi7.cu
+#include <stdio.h>
 #include <cuda.h>
 #include <cuda_runtime.h>
 #include <cuda_call.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
 #include <math.h>
