052281d6aeb15897654b067717c69189f584b20d
intersense <zgw1982@gmail.com>
2016-02-06 14:06:36 -0700

    host device data transfer

---
+++ bandwidthtest/host-device-transfer.cu
@@ -0,0 +1,16 @@
+#include <cuda.h>
+#include <cuda_runtime.h>
+
+int main(){
+    const unsigned int X = 1048576; // 1 Megabyte
+    const unsigned int bytes = X * sizeof(int);
+    int *hostArray = (int*) malloc(bytes);
+    int *deviceArray;
+    cudaMalloc((int**)&deviceArray, bytes);
+    memset(hostArray, 0, bytes);
+    cudaMemcpy(deviceArray, hostArray, bytes, cudaMemcpyHostToDevice);
+    cudaMemcpy(hostArray, deviceArray, bytes, cudaMemcpyDeviceToHost);
+    cudaFree(deviceArray);
+
+    return 0;
+}
