1040f7f684ef90d920490f326bb8ffe914b2f3c1
intersense <zgw1982@gmail.com>
2016-04-15 14:26:51 -0600

    shared memory version modify

--- include/jacobi7_cuda_shared.h
+++ include/jacobi7_cuda_shared.h
@@ -26,10 +26,10 @@ __global__ void jacobi3d_7p_shmem_adam(float * d_in, float * d_out, const int nx
   front = d_in[CURRENT_G + nx*ny]; 
 
   // Load halo region into shared memory
-  if(tx == 1 && ix > 0)       s_data[CURRENT_S - 1]  = d_in[CURRENT_G - 1];
-  if(tx == bx-2 && ix < nx-1) s_data[CURRENT_S + 1]  = d_in[CURRENT_G + 1];
-  if(ty == 1 && iy > 0)       s_data[CURRENT_S - bx] = d_in[CURRENT_G - nx];
-  if(ty == by-2 && iy < ny-1) s_data[CURRENT_S + bx] = d_in[CURRENT_G + nx];
+  if(tx > 1 && tx < bx-2 && ix > 0 && ix < nx-1) s_data[CURRENT_S - 1]  = d_in[CURRENT_G - 1];
+  if(tx > 1 && tx < bx-2 && ix > 0 && ix < nx-1) s_data[CURRENT_S + 1]  = d_in[CURRENT_G + 1];
+  if(ty > 1 && ty < by-2 && iy > 0 && iy < ny-1) s_data[CURRENT_S - bx] = d_in[CURRENT_G - nx];
+  if(ty > 1 && ty < by-2 && iy > 0 && iy < ny-1) s_data[CURRENT_S + bx] = d_in[CURRENT_G + nx];
   __syncthreads();
 
   // Load shared memory into registers
@@ -55,10 +55,17 @@ __global__ void jacobi3d_7p_shmem_adam(float * d_in, float * d_out, const int nx
     front = d_in[CURRENT_G + nx*ny];
 
     // Load halo region into shared memory
+
+    if(tx > 1 && tx < bx-2 && ix > 0 && ix < nx-1) s_data[CURRENT_S - 1]  = d_in[CURRENT_G - 1];
+    if(tx > 1 && tx < bx-2 && ix > 0 && ix < nx-1) s_data[CURRENT_S + 1]  = d_in[CURRENT_G + 1];
+    if(ty > 1 && ty < by-2 && iy > 0 && iy < ny-1) s_data[CURRENT_S - bx] = d_in[CURRENT_G - nx];
+    if(ty > 1 && ty < by-2 && iy > 0 && iy < ny-1) s_data[CURRENT_S + bx] = d_in[CURRENT_G + nx];
+    /*
     if(tx == 1 && ix > 0)       s_data[CURRENT_S - 1]  = d_in[CURRENT_G - 1];
     if(tx == bx-2 && ix < nx-1) s_data[CURRENT_S + 1]  = d_in[CURRENT_G + 1];
     if(ty == 1 && iy > 0)       s_data[CURRENT_S - bx] = d_in[CURRENT_G - nx];
     if(ty == by-2 && iy < ny-1) s_data[CURRENT_S + bx] = d_in[CURRENT_G + nx];
+    */
     __syncthreads();
 
     // Load shared memory into registers
