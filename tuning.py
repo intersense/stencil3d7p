ef0fdaf096a807537664b28a6e668fd837de5136
intersense <zgw1982@gmail.com>
2016-04-17 23:02:00 -0600

    tunning script

--- tuning.py
+++ tuning.py
@@ -3,10 +3,13 @@
 import subprocess
 import sys
 
-test = input("Input the size of the stencil grid:")
+nx = input("Input the size of the stencil grid:")
+bx = input("Input the block size of X:")
+by = input("Input the block size of Y:")
 command = "./verify-adam.o "
 command += "0 "
-command += str(test) + " " + str(test) + " " + str(test) + " "
-command += "1 1"
+command += str(nx) + " " + str(nx) + " " + str(nx) + " "
+command += str(bx) + " " + str(by) + " "
+command += "1"
 proc = subprocess.Popen(command, shell=True)
 proc.wait()
