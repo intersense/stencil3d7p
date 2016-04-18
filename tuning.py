fcfb7aa564ce43b8acc6f6ae36e12c4918a1a71f
intersense <zgw1982@gmail.com>
2016-04-17 22:59:12 -0600

    tunning script

--- tuning.py
+++ tuning.py
@@ -6,7 +6,7 @@ import sys
 test = input("Input the size of the stencil grid:")
 command = "./verify-adam.o "
 command += "0 "
-command += test + " " + test + " " + test + " "
+command += str(test) + " " + str(test) + " " + str(test) + " "
 command += "1 1"
 proc = subprocess.Popen(command, shell=True)
 proc.wait()
