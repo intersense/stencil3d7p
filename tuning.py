b1909951e220e9e8be4d47e638ece4627ace88c4
intersense <zgw1982@gmail.com>
2016-04-17 23:04:14 -0600

    tunning script

--- tuning.py
+++ tuning.py
@@ -2,14 +2,15 @@
 
 import subprocess
 import sys
-
-nx = input("Input the size of the stencil grid:")
-bx = input("Input the block size of X:")
-by = input("Input the block size of Y:")
-command = "./verify-adam.o "
-command += "0 "
-command += str(nx) + " " + str(nx) + " " + str(nx) + " "
-command += str(bx) + " " + str(by) + " "
-command += "1"
-proc = subprocess.Popen(command, shell=True)
-proc.wait()
+for bx in [256, 128]:
+    for by in [1, 2]:
+        #nx = input("Input the size of the stencil grid:")
+        #bx = input("Input the block size of X:")
+        #by = input("Input the block size of Y:")
+        command = "./verify-adam.o "
+        command += "0 "
+        command += str(nx) + " " + str(nx) + " " + str(nx) + " "
+        command += str(bx) + " " + str(by) + " "
+        command += "1"
+        proc = subprocess.Popen(command, shell=True)
+        proc.wait()
