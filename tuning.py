456ae7199a0b4404c598234c5c1bce181cb4ca04
intersense <zgw1982@gmail.com>
2016-04-17 22:54:50 -0600

    tunning script

--- tuning.py
+++ tuning.py
@@ -4,5 +4,5 @@ import subprocess
 import sys
 
 
-proc = subprocess.Popen("verify-adam.o 0 256 256 256 156 1 1", shell=True)
+proc = subprocess.Popen("./verify-adam.o 0 256 256 256 156 1 1", shell=True)
 proc.wait()
