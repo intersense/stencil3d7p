99f2bb14d1354a37625ed9ccfcc3f8d10a0d6baa
intersense <zgw1982@gmail.com>
2016-04-18 17:24:51 -0600

    3d + temporal blocking

--- include/jacobi7_cuda_3d.h
+++ include/jacobi7_cuda_3d.h
@@ -63,6 +63,8 @@ __global__ void jacobi3d_7p_shmem_3d(float *d_in, float *d_out, const int nx, co
 }
 
 // 3D + temporal blocking
+// 1: not only loading the data to shared memory, but computing them
+// 2: compute like non-temporal version
 __global__ void jacobi3d_7p_shmem_3d_temporal(float *d_in, float *d_out, const int nx, const int ny, const int nz, float fac){
   // block dimension
   const int bx = blockDim.x;
@@ -70,9 +72,9 @@ __global__ void jacobi3d_7p_shmem_3d_temporal(float *d_in, float *d_out, const i
   const int bz = blockDim.z;
 
   //shared memory dimension
-  const int share_x = bx + 4;
-  const int share_y = by + 4;
-  const int share_z = bz + 4;
+  const int share_x = bx + 2;
+  const int share_y = by + 2;
+  const int share_z = bz + 2;
 
   // thread global dimension
   const int ix = threadIdx.x + blockIdx.x * bx;
@@ -80,12 +82,12 @@ __global__ void jacobi3d_7p_shmem_3d_temporal(float *d_in, float *d_out, const i
   const int iz = threadIdx.z + blockIdx.z * bz;
 
   // grid points to be computed, dimensions
-  const int s_x = threadIdx.x;
-  const int s_y = threadIdx.y;
-  const int s_z = threadIdx.z;
+  const int s_x = threadIdx.x + 1;
+  const int s_y = threadIdx.y + 1;
+  const int s_z = threadIdx.z + 1;
 
-  int CURRENT_G = ix + nx * (iy + ny * iz);
-  int CURRENT_S = s_x + share_x * (s_y + share_y * s_z);
+  int C_G = ix + nx * (iy + ny * iz);
+  int C_S = s_x + share_x * (s_y + share_y * s_z);
 
 
   extern __shared__ float s_data[];
@@ -97,157 +99,59 @@ __global__ void jacobi3d_7p_shmem_3d_temporal(float *d_in, float *d_out, const i
 
   float temp;
 
-  // Load current, front, and back nodes into shared and register memory
-  curr  = s_data[CURRENT_S] = d_in[CURRENT_G];
- 
-  // 1
+  bool boundary1 = ix > 1 && ix < nx-2 && iy > 1 && iy < ny - 2 && iz > 1 && iz < nz - 2;
+  bool boundary2 = ix > 0 && ix < nx-1 && iy > 0 && iy < ny - 1 && iz > 0 && iz < nz - 1;
+  // Load c, front, and back nodes into shared and register memory
+  if (boundary1){
+    curr  = s_data[C_S] = d_in[C_G];
+  }  
+  
   // Load halo region into shared memory
-  // left
-  if (s_x == 0 && ix > 0)         s_data[CURRENT_S - 1]  = d_in[CURRENT_G - 1];
-  if (s_x == 0 && ix > 1)         s_data[CURRENT_S - 2]  = d_in[CURRENT_G - 2];
-
-  // right
-  if (s_x == bx-1 && ix < nx-1)   s_data[CURRENT_S + 1]  = d_in[CURRENT_G + 1];
-  if (s_x == bx-1 && ix < nx-2)   s_data[CURRENT_S + 2]  = d_in[CURRENT_G + 2];
-
-  // top
-  if (s_y == 0 && iy > 0)         s_data[CURRENT_S - share_x] = d_in[CURRENT_G - nx];
-  if (s_y == 0 && iy > 1)         s_data[CURRENT_S - 2 * share_x] = d_in[CURRENT_S - 2 * nx];
-
-  // down
-  if (s_y == by-1 && iy < ny-1)   s_data[CURRENT_S + share_x] = d_in[CURRENT_G + nx];
-  if (s_y == by-1 && iy < ny-2)   s_data[CURRENT_S + 2 * share_x] = d_in[CURRENT_G + 2 * nx];
-
-  // front
-  if (s_z == 0 && iz > 0)         s_data[CURRENT_S - share_x * share_y] = d_in[CURRENT_G - nx * ny];
-  if (s_z == 0 && iz > 1)         s_data[CURRENT_S - 2 * share_x * share_y] = d_in[CURRENT_G - 2 * nx * ny];
-
-  // back
-  if (s_z == bz-1 && iz < nz-1)   s_data[CURRENT_S + share_x * share_y] = d_in[CURRENT_G + nx * ny];
-  if (s_z == bz-1 && iz < nz-2)    s_data[CURRENT_S + 2 * share_x * share_y] = d_in[CURRENT_G + 2 * nx * ny];
-
-  // top-left
-  if (s_x == 0 && s_y == 0 && ix > 0 && iy > 0) s_data[CURRENT_S - share_x - 1] = d_in[CURRENT_G - nx - 1];
-  if (s_x == 0 && s_y == 0 && ix > 1 && iy > 0) s_data[CURRENT_S - share_x - 2] = d_in[CURRENT_G - nx - 2];
-  if (s_x == 0 && s_y == 0 && ix > 0 && iy > 1) s_data [CURRENT_S - 2 * share_x - 1] = d_in[CURRENT_S - 2 * nx -1];
-
-  // top-right
-  if (s_x == bx-1 && s_y == 0 && ix < nx-1 && iy > 0) s_data[CURRENT_S - share_x + 1] = d_in[CURRENT_G - nx + 1];
-  if (s_x == bx-1 && s_y == 0 && ix < nx-2 && iy > 0) s_data[CURRENT_S - share_x + 2] = d_in[CURRENT_G - nx + 2];
-  if (s_x == bx-1 && s_y == 0 && ix < nx-1 && iy > 1) s_data[CURRENT_S - 2 * share_x + 1] = d_in[CURRENT_G - 2 * nx + 1];
-
-  // left-down
-  if (s_x == 0 && s_y == by-1 && ix > 0 && iy < ny-1) s_data[CURRENT_S + share_x - 1] = d_in[CURRENT_G + nx - 1];
-  if (s_x == 0 && s_y == by-1 && ix > 1 && iy < ny-1) s_data[CURRENT_S + share_x - 2] = d_in[CURRENT_G + nx - 2];
-  if (s_x == 0 && s_y == by-1 && ix > 0 && iy < ny-2) s_data[CURRENT_S + 2 * share_x -1] = d_in[CURRENT_G + 2 * nx - 1];
-
-  // right-down
-  if (s_x == bx-1 && s_y == by-1 && ix < nx-1 && iy < ny-1) s_data[CURRENT_S + share_x + 1] = d_in[CURRENT_G + nx + 1];
-  if (s_x == bx-1 && s_y == by-1 && ix < nx-2 && iy < ny-1) s_data[CURRENT_S + share_x + 2] = d_in[CURRENT_G + nx + 2];
-  if (s_x == bx-1 && s_y == by-1 && ix < nx-1 && iy < ny-2) s_data[CURRENT_S + 2*share_x + 1] = d_in[CURRENT_G + 2*nx + 1];
-
-  __syncthreads();
-  // the core thread block
-  if(ix > 0 && ix < nx-1 & iy > 0 && iy < ny-1 && iz > 0 && iz < nz -1)
-  {
-    // Load shared memory into registers
-    curr  = s_data[CURRENT_S];
-    right = s_data[CURRENT_S + 1];
-    left  = s_data[CURRENT_S - 1];
-    up    = s_data[CURRENT_S - share_x];
-    down  = s_data[CURRENT_S + share_x];
-    front = s_data[CURRENT_S - share_x * share_y];
-    back  = s_data[CURRENT_S + share_x * share_y];
-    s_data[CURRENT_S] = right + left + up + down + front + back - curr * fac;
-    // left halo
-    if (s_x == 0 && ix > 1){
-      curr  = s_data[CURRENT_S - 1];
-      right = s_data[CURRENT_S - 1 + 1];
-      left  = s_data[CURRENT_S - 1 - 1];
-      up    = s_data[CURRENT_S - 1 - share_x];
-      down  = s_data[CURRENT_S - 1 + share_x];
-      front = s_data[CURRENT_S - 1 - share_x * share_y];
-      back  = s_data[CURRENT_S - 1 + share_x * share_y];
-      s_data[CURRENT_S - 1] = right + left + up + down + front + back - curr * fac;
+  if(s_x == 1 && ix > 0) {
+    // condition: C_G - 1 - 1, C_G - 1 + 1, C_G - 1 - nx, C_G - 1 + nx, C_G - 1 - nx * ny, C_G - 1 + nx * ny and C_G - 1
+    if (boundary1){
+      s_data[C_S - 1] = d_in[C_G - 1 - 1] + d_in[C_G - 1 + 1] + d_in[C_G - 1 - nx] + d_in[C_G - 1 + nx] + d_in[C_G - 1 - nx * ny] + d_in[C_G - 1 + nx * ny] - fac * d_in[C_G - 1];
     }
-    // right halo
-    if (s_x == bx-1 && ix < nx - 2){
-      curr  = s_data[CURRENT_S + 1];
-      right = s_data[CURRENT_S + 1 + 1];
-      left  = s_data[CURRENT_S + 1 - 1];
-      up    = s_data[CURRENT_S + 1 - share_x];
-      down  = s_data[CURRENT_S + 1 + share_x];
-      front = s_data[CURRENT_S + 1 - share_x * share_y];
-      back  = s_data[CURRENT_S + 1 + share_x * share_y];
-      s_data[CURRENT_S + 1] = right + left + up + down + front + back - curr * fac;
-    }
-    // top halo
-    if (s_y == 0 && iy > 1){
-      curr  = s_data[CURRENT_S - share_x];
-      right = s_data[CURRENT_S - share_x + 1];
-      left  = s_data[CURRENT_S - share_x - 1];
-      up    = s_data[CURRENT_S - share_x - share_x];
-      down  = s_data[CURRENT_S - share_x + share_x];
-      front = s_data[CURRENT_S - share_x - share_x * share_y];
-      back  = s_data[CURRENT_S - share_x + share_x * share_y];
-      s_data[CURRENT_S - share_x] = right + left + up + down + front + back - curr * fac;
-    }
-    // down halo
-    if (s_y == by-1 && iy < ny - 2){
-      curr  = s_data[CURRENT_S + share_x];
-      right = s_data[CURRENT_S + share_x + 1];
-      left  = s_data[CURRENT_S + share_x - 1];
-      up    = s_data[CURRENT_S + share_x - share_x];
-      down  = s_data[CURRENT_S + share_x + share_x];
-      front = s_data[CURRENT_S + share_x - share_x * share_y];
-      back  = s_data[CURRENT_S + share_x + share_x * share_y];
-      s_data[CURRENT_S + share_x] = right + left + up + down + front + back - curr * fac;
-    }
-    // front halo
-    if (s_z == 0 && iz > 1){
-      curr  = s_data[CURRENT_S - share_x*share_y];
-      right = s_data[CURRENT_S - share_x*share_y + 1];
-      left  = s_data[CURRENT_S - share_x*share_y - 1];
-      up    = s_data[CURRENT_S - share_x*share_y - share_x];
-      down  = s_data[CURRENT_S - share_x*share_y + share_x];
-      front = s_data[CURRENT_S - share_x*share_y - share_x * share_y];
-      back  = s_data[CURRENT_S - share_x*share_y + share_x * share_y];
-      s_data[CURRENT_S - share_x*share_y] = right + left + up + down + front + back - curr * fac;
-    }
-    // back halo
-    if (s_z == bz-1 && iz < nz-2){
-      curr  = s_data[CURRENT_S + share_x*share_y];
-      right = s_data[CURRENT_S + share_x*share_y + 1];
-      left  = s_data[CURRENT_S + share_x*share_y - 1];
-      up    = s_data[CURRENT_S + share_x*share_y - share_x];
-      down  = s_data[CURRENT_S + share_x*share_y + share_x];
-      front = s_data[CURRENT_S + share_x*share_y - share_x * share_y];
-      back  = s_data[CURRENT_S + share_x*share_y + share_x * share_y];
-      s_data[CURRENT_S + share_x*share_y] = right + left + up + down + front + back - curr * fac;
-    }
-
   }
-
+  if(s_x == share_x-2 && ix < nx-1){
+    if(boundary1){
+      s_data[C_S + 1] = d_in[C_G + 1 - 1] + d_in[C_G + 1 + 1] + d_in[C_G + 1 - nx] + d_in[C_G + 1 + nx] + d_in[C_G + 1 - nx * ny] + d_in[C_G + 1 + nx * ny] - fac * d_in[C_G + 1];
+    }
+  }
+  if(s_y == 1 && iy > 0){
+    if (boundary1){
+      s_data[C_S - share_x] = d_in[C_G - nx - 1] + d_in[C_G - nx + 1] + d_in[C_G - nx - nx] + d_in[C_G - nx + nx] + d_in[C_G - nx - nx * ny] + d_in[C_G - nx + nx * ny] - fac * d_in[C_G - nx];
+    } 
+  }
+  if(s_y == share_y-2 && iy < ny-1){
+    if (boundary1){
+      s_data[C_S + share_x] = d_in[C_G + nx - 1] + d_in[C_G + nx + 1] + d_in[C_G + nx - nx] + d_in[C_G + nx + nx] + d_in[C_G + nx - nx * ny] + d_in[C_G + nx + nx * ny] - fac * d_in[C_G + nx];
+    }
+  }
+  if(s_z == 1 && iz >0){
+    if (boundary1){
+      s_data[C_S - share_x * share_y] = d_in[C_G - nx * ny - 1] + d_in[C_G - nx * ny + 1] + d_in[C_G - nx * ny - nx] + d_in[C_G - nx * ny + nx] + d_in[C_G - nx * ny - nx * ny] + d_in[C_G - nx * ny + nx * ny] - fac * d_in[C_G - nx * ny];
+    }
+  }
+  if(s_z == share_z-2 && iz < nz-1){
+    if (boundary1){
+      s_data[C_S + share_x * share_y] = d_in[C_G + nx * ny - 1] + d_in[C_G + nx * ny + 1] + d_in[C_G + nx * ny - nx] + d_in[C_G + nx * ny + nx] + d_in[C_G + nx * ny - nx * ny] + d_in[C_G + nx * ny + nx * ny] - fac * d_in[C_G + nx * ny];
+    }
+  }
   __syncthreads();
 
-  // 2
-  if (ix > 0 && ix < nx-1 & iy > 0 && iy < ny-1 && iz > 0 && iz < nz -1)
+  // Load shared memory into registers
+  curr  = s_data[C_S];
+  right = s_data[C_S + 1];
+  left  = s_data[C_S - 1];
+  up    = s_data[C_S - share_x];
+  down  = s_data[C_S + share_x];
+  front = s_data[C_S - share_x * share_y];
+  back  = s_data[C_S + share_x * share_y];
+
+  if(boundary2)
   {
-    curr  = s_data[CURRENT_S];
-    right = s_data[CURRENT_S + 1];
-    left  = s_data[CURRENT_S - 1];
-    up    = s_data[CURRENT_S - share_x];
-    down  = s_data[CURRENT_S + share_x];
-    front = s_data[CURRENT_S - share_x * share_y];
-    back  = s_data[CURRENT_S + share_x * share_y]; 
     temp = right + left + up + down + front + back - curr * fac;
-    d_out[CURRENT_G] = temp;
+    d_out[C_G] = temp;
   }
-
-
-  /*if(ix > 0 && ix < nx-1 & iy > 0 && iy < ny-1 && iz > 0 && iz < nz -1)
-  {
-    temp = right + left + up + down + front + back - curr * fac;
-    d_out[CURRENT_G] = temp;
-  }*/
 }
