b0b0fd1806f4d94137debea8f220ed6233b0440a
intersense <zgw1982@gmail.com>
2016-04-18 12:28:51 -0600

    3d version shared modify function call

--- tuning_3d.py
+++ tuning_3d.py
@@ -8,7 +8,7 @@ proc.wait()
 nx = input("Input the size of the stencil grid:")
 for bx in [4, 8, 16, 32]:
     for by in [4, 8, 16, 32]:
-        for bz = in [4, 8, 16, 32]:
+        for bz in [4, 8, 16, 32]:
             if (bx*by*bz<=1024):
                 #nx = input("Input the size of the stencil grid:")
                 #bx = input("Input the block size of X:")
