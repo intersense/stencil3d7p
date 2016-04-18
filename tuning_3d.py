2dcfe4623defb3958b5d785bd3971699f53b634d
intersense <zgw1982@gmail.com>
2016-04-18 12:30:15 -0600

    3d version shared modify function call

--- tuning_3d.py
+++ tuning_3d.py
@@ -2,7 +2,7 @@
 
 import subprocess
 import sys
-command = "make verify-shared-3d"
+command = "make verify-shmem-3d"
 proc = subprocess.Popen(command, shell=True)
 proc.wait()
 nx = input("Input the size of the stencil grid:")
@@ -13,7 +13,7 @@ for bx in [4, 8, 16, 32]:
                 #nx = input("Input the size of the stencil grid:")
                 #bx = input("Input the block size of X:")
                 #by = input("Input the block size of Y:")
-                command = "./verify-shared-3d.o "
+                command = "./verify-shmem-3d.o "
                 command += str(nx) + " " + str(nx) + " " + str(nx) + " "
                 command += str(bx) + " " + str(by) + " " + str(bz) + " "
                 command += "1"
