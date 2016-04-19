44d0956329f98bed7074ef2ccfa9c2d2ccfe953c
intersense <zgw1982@gmail.com>
2016-04-19 10:53:18 -0600

    2.5d blocking

--- include/jacobi7_cuda_temporal.h
+++ include/jacobi7_cuda_temporal.h
@@ -48,7 +48,7 @@ __global__ void jacobi3d_7p_25d(float * d_in, float * d_out, const int nx, const
   // compute k_0 and store in k_0
   if (boundary_c && boundary_k){
     int C_G_0 = C_G - nx * ny;
-    k_0[C_S] = d_in[C_G_0 - 1] + d_in[C_G_0 + 1] + d_in[C_G_0 - nx] + d_in[C_G_0 + nx] + d_in[C_G_0 - nx * ny] + d_in[C_G_0 + nx * ny] - fac * d_in[C_G_0]; 
+    k_0[C_S] = d_in[C_G_0];
 
     // compute k_1 and store in k_1
     k_1[C_S] = d_in[C_G - 1] + d_in[C_G + 1] + d_in[C_G - nx] + d_in[C_G + nx] + d_in[C_G - nx * ny] + d_in[C_G + nx * ny] - fac * d_in[C_G];
