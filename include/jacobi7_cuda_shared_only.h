0da4b5d5bfdd98ea66e77b190c1f3c60cd2bf0fd
intersense <zgw1982@gmail.com>
2016-05-21 16:38:14 -0600

    pure shared blocking

--- include/jacobi7_cuda_shared_only.h
+++ include/jacobi7_cuda_shared_only.h
@@ -30,7 +30,8 @@ __global__ void jacobi3d_7p_shmem_only(float * d_in, float * d_out, const int nx
   if(tx == bx && ix < nx-1)    s_data[CURRENT_S + 1]   = d_in[CURRENT_G + 1];
   if(ty == 1  && iy > 0)       s_data[CURRENT_S - x_s] = d_in[CURRENT_G - nx];
   if(ty == by && iy < ny-1)    s_data[CURRENT_S + x_s] = d_in[CURRENT_G + nx];
-  /*
+
+  /* top and down neen not load the halo, just let current plane load the halo
   // Load halo region into shared memory (top)
   if(tx == 1 && ix > 0)       s_data[CURRENT_S  + xy_s - 1]   = d_in[CURRENT_G + nx*ny - 1];
   if(tx == bx && ix < nx-1)   s_data[CURRENT_S  + xy_s + 1]   = d_in[CURRENT_G + nx*ny + 1];
@@ -54,28 +55,82 @@ __global__ void jacobi3d_7p_shmem_only(float * d_in, float * d_out, const int nx
     CURRENT_G += nx*ny;
     __syncthreads();
     // Re-use data already in shared mem to move forward
-    /*down  = curr;
-    curr  = s_data[CURRENT_S] = top;
-    top = d_in[CURRENT_G + nx*ny];
+    /*
+    down=curr
+    curr=top
+    top=load from glmem
     */
+    // moving data in shared memory is an overhead
+    /*
     //down
     s_data[CURRENT_S-xy_s] = s_data[CURRENT_S];
     //curr
     s_data[CURRENT_S] = s_data[CURRENT_S+xy_s];
-    
     //top
     s_data[CURRENT_S+xy_s] = d_in[CURRENT_G + nx*ny];
+
     // Load halo region into shared memory
     if(tx == 1 && ix > 0)     s_data[CURRENT_S - 1]  = d_in[CURRENT_G - 1];
     if(tx == bx && ix < nx-1) s_data[CURRENT_S + 1]  = d_in[CURRENT_G + 1];
     if(ty == 1 && iy > 0)     s_data[CURRENT_S - x_s] = d_in[CURRENT_G - nx];
     if(ty == by && iy < ny-1) s_data[CURRENT_S + x_s] = d_in[CURRENT_G + nx];
     __syncthreads();
-    
     // Perform computation and write to output grid (excluding edge nodes)
     if(ix > 0 && ix < nx-1 && iy > 0 && iy < ny-1)
     {
       d_out[CURRENT_G] = s_data[CURRENT_S+1] + s_data[CURRENT_S-1] + s_data[CURRENT_S-x_s] + s_data[CURRENT_S+x_s] + s_data[CURRENT_S-xy_s] + s_data[CURRENT_S+xy_s] - fac * s_data[CURRENT_S];
     }
-  }
+    */
+    // not moving data between different planes
+    if (k%3==1)//(c,d,t)
+    {
+        //top
+        s_data[CURRENT_S-xy_s] = d_in[CURRENT_G + nx*ny]; 
+
+        // Load halo region into shared memory
+        if(tx == 1 && ix > 0)     s_data[CURRENT_S + xy_s - 1]  = d_in[CURRENT_G - 1];
+        if(tx == bx && ix < nx-1) s_data[CURRENT_S + xy_s + 1]  = d_in[CURRENT_G + 1];
+        if(ty == 1 && iy > 0)     s_data[CURRENT_S + xy_s - x_s] = d_in[CURRENT_G - nx];
+        if(ty == by && iy < ny-1) s_data[CURRENT_S + xy_s + x_s] = d_in[CURRENT_G + nx];
+        __syncthreads();
+        // Perform computation and write to output grid (excluding edge nodes)
+        if(ix > 0 && ix < nx-1 && iy > 0 && iy < ny-1)
+        {
+          d_out[CURRENT_G] = s_data[CURRENT_S+xy_s+1] + s_data[CURRENT_S+xy_s-1] + s_data[CURRENT_S+xy_s-x_s] + s_data[CURRENT_S+xy_s+x_s] + s_data[CURRENT_S+xy_s-xy_s] + s_data[CURRENT_S+xy_s+xy_s] - fac * s_data[CURRENT_S+xy_s];
+        }
+    }
+    if (k%3==2)//(d,t,c)
+    {
+        //top
+        s_data[CURRENT_S] = d_in[CURRENT_G + nx*ny]; 
+
+        // Load halo region into shared memory
+        if(tx == 1 && ix > 0)     s_data[CURRENT_S - xy_s - 1]  = d_in[CURRENT_G - 1];
+        if(tx == bx && ix < nx-1) s_data[CURRENT_S - xy_s + 1]  = d_in[CURRENT_G + 1];
+        if(ty == 1 && iy > 0)     s_data[CURRENT_S - xy_s - x_s] = d_in[CURRENT_G - nx];
+        if(ty == by && iy < ny-1) s_data[CURRENT_S - xy_s + x_s] = d_in[CURRENT_G + nx];
+        __syncthreads();
+        // Perform computation and write to output grid (excluding edge nodes)
+        if(ix > 0 && ix < nx-1 && iy > 0 && iy < ny-1)
+        {
+          d_out[CURRENT_G] = s_data[CURRENT_S-xy_s+1] + s_data[CURRENT_S-xy_s-1] + s_data[CURRENT_S-xy_s-x_s] + s_data[CURRENT_S-xy_s+x_s] + s_data[CURRENT_S-xy_s-xy_s] + s_data[CURRENT_S-xy_s+xy_s] - fac * s_data[CURRENT_S-xy_s];
+        }
+    }
+    if (k%3==0)//(d,t,c)
+    {
+        //top
+        s_data[CURRENT_S+xy_s] = d_in[CURRENT_G + nx*ny]; 
+
+        // Load halo region into shared memory
+        if(tx == 1 && ix > 0)     s_data[CURRENT_S - 1]  = d_in[CURRENT_G - 1];
+        if(tx == bx && ix < nx-1) s_data[CURRENT_S + 1]  = d_in[CURRENT_G + 1];
+        if(ty == 1 && iy > 0)     s_data[CURRENT_S - x_s] = d_in[CURRENT_G - nx];
+        if(ty == by && iy < ny-1) s_data[CURRENT_S + x_s] = d_in[CURRENT_G + nx];
+        __syncthreads();
+        // Perform computation and write to output grid (excluding edge nodes)
+        if(ix > 0 && ix < nx-1 && iy > 0 && iy < ny-1)
+        {
+          d_out[CURRENT_G] = s_data[CURRENT_S+1] + s_data[CURRENT_S-1] + s_data[CURRENT_S-x_s] + s_data[CURRENT_S+x_s] + s_data[CURRENT_S-xy_s] + s_data[CURRENT_S+xy_s] - fac * s_data[CURRENT_S];
+        }
+    }
 }
